794b3b81140c31fedda79cac2b6021a0bcaa7351
Starry-Hu <1756487384@qq.com>
2019-04-16 17:45:36 +0800

    数据对接

---
+++ duijie/js/common.js
@@ -0,0 +1,28 @@
+// tokenValue = 'bd76d09b-00f4-4c0f-ad6e-3f0f2fca6adb';
+basePath = "http://s1.anylinkcloud.com:8600";
+tokenValue = '09957541-1182-45ce-adb9-8ba6ff7eb301';
+
+// 登录
+// var jsonString = {
+//     name: "duobiy",
+//     password: "duobiy01",
+//     hash: "test"
+// }
+
+// $.ajax({
+//     type: "post",
+//     url: basePath + "/user/login",
+//     contentType: "application/json",
+//     dataType: "json",
+//     async: false,
+//     data: JSON.stringify(jsonString),
+//     // data: jsonString,
+//     success: function(response) {
+//         console.log(response);
+//         if (response.status == 100) {
+//             tokenValue = response.data;
+//         } else {
+//             alert(response.msg);
+//         }
+//     }
+// });
