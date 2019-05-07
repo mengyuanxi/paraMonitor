f25bdbe24bb28de6faa2b9aaa6520f2d75fd64ca
Starry-Hu <1756487384@qq.com>
2019-05-07 22:01:35 +0800

    更新，对接数据

--- duijie/js/common.js
+++ duijie/js/common.js
@@ -1,28 +1,29 @@
-// tokenValue = 'bd76d09b-00f4-4c0f-ad6e-3f0f2fca6adb';
+var cookie = {
+    set: function(key, val, time) { //设置cookie方法
+        var date = new Date(); //获取当前时间
+        var expiresDays = time; //将date设置为n天以后的时间
+        date.setTime(date.getTime() + expiresDays * 24 * 3600 * 1000); //格式化为cookie识别的时间
+        document.cookie = key + "=" + val + ";expires=" + date.toGMTString(); //设置cookie
+    },
+    get: function(key) { //获取cookie方法
+        /*获取cookie参数*/
+        var getCookie = document.cookie.replace(/[ ]/g, ""); //获取cookie，并且将获得的cookie格式化，去掉空格字符
+        var arrCookie = getCookie.split(";") //将获得的cookie以"分号"为标识 将cookie保存到arrCookie的数组中
+        var tips; //声明变量tips
+        for (var i = 0; i < arrCookie.length; i++) { //使用for循环查找cookie中的tips变量
+            var arr = arrCookie[i].split("="); //将单条cookie用"等号"为标识，将单条cookie保存为arr数组
+            if (key == arr[0]) { //匹配变量名称，其中arr[0]是指的cookie名称，如果该条变量为tips则执行判断语句中的赋值操作
+                tips = arr[1]; //将cookie的值赋给变量tips
+                break; //终止for循环遍历
+            }
+        }
+        return tips;
+    },
+    delete: function(key) { //删除cookie方法
+        var date = new Date(); //获取当前时间
+        date.setTime(date.getTime() - 10000); //将date设置为过去的时间
+        document.cookie = key + "=v; expires =" + date.toGMTString(); //设置cookie
+    }
+}
+
 basePath = "http://s1.anylinkcloud.com:8600";
-tokenValue = '09957541-1182-45ce-adb9-8ba6ff7eb301';
-
-// 登录
-// var jsonString = {
-//     name: "duobiy",
-//     password: "duobiy01",
-//     hash: "test"
-// }
-
-// $.ajax({
-//     type: "post",
-//     url: basePath + "/user/login",
-//     contentType: "application/json",
-//     dataType: "json",
-//     async: false,
-//     data: JSON.stringify(jsonString),
-//     // data: jsonString,
-//     success: function(response) {
-//         console.log(response);
-//         if (response.status == 100) {
-//             tokenValue = response.data;
-//         } else {
-//             alert(response.msg);
-//         }
-//     }
-// });
